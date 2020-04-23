002c658ca755bc6bb8f1a912a6306ff07f4d7664
Li-Zhijian2000 <lzj01071508@buaa.edu.cn>
2020-04-23 23:29:09 +0800

    add: rent-model & rent-security

---
+++ rent-parent/rent-model/src/test/java/com/javaweb/rent/rentmodel/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb.rent.rentmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
